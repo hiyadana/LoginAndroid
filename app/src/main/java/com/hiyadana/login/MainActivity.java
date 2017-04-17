05daa0009674c791a97457acc113dc5006c26ebd
hiyadana <windypip@gmail.com>
2017-04-17 09:44:55 +0900

    update for picture select function from album or camera

--- app/src/main/java/com/hiyadana/login/MainActivity.java
+++ app/src/main/java/com/hiyadana/login/MainActivity.java
@@ -2,13 +2,19 @@ package com.hiyadana.login;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -22,9 +28,13 @@ import com.hiyadana.login.model.User;
 import com.hiyadana.login.network.NetworkUtil;
 import com.hiyadana.login.utils.Constants;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.Locale;
+//import java.util.log;
 
 import retrofit2.adapter.rxjava.HttpException;
 import rx.android.schedulers.AndroidSchedulers;
@@ -38,16 +48,27 @@ import static com.hiyadana.login.utils.Validation.validatePassword;
 public class MainActivity extends AppCompatActivity {
     final static public String TAG = AppCompatActivity.class.getSimpleName();
 
+    private static final int PICK_FROM_CAMERA = 0;
+    private static final int PICK_FROM_ALBUM = 1;
+    private static final int CROP_FROM_IMAGE = 2;
+
     private NumberPicker mNp;
     private Button mButtonRegister;
     private Button mButtonLogin;
     private Button mButtonLogout;
+    private Button mButtonPicture;
+    private Button mButtonCamera;
+    private ImageView mImageView;
 
     private String mLoginNickname;
     private String mLoginPassword;
 
     private CompositeSubscription mSubscriptions;
     private SharedPreferences mSharedPreferences;
+
+    private Uri mImageUri;
+
+    private String mAbsoluteImagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,9 +78,14 @@ public class MainActivity extends AppCompatActivity {
             setContentView(R.layout.activity_main);
             mButtonRegister = (Button)findViewById(R.id.btn_register);
             mButtonLogin = (Button)findViewById(R.id.btn_login);
+            mButtonPicture = (Button)findViewById(R.id.btn_picture);
+            mButtonCamera = (Button)findViewById(R.id.btn_camera);
+            mImageView = (ImageView)findViewById(R.id.img_profile);
             initLoginView();
             mButtonRegister.setOnClickListener(v -> onRegister());
             mButtonLogin.setOnClickListener(v -> onLogin());
+            mButtonPicture.setOnClickListener(v -> onPicture());
+            mButtonCamera.setOnClickListener(v -> onCamera());
         }
         else {
             setContentView(R.layout.activity_home);
@@ -77,6 +103,95 @@ public class MainActivity extends AppCompatActivity {
         mNp.setMaxValue(2017);
         mNp.setValue(2000);
     }
+    private void onPicture() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+    private void onCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        String filename = "login_"+String.valueOf(System.currentTimeMillis())+".jpg";
+        mImageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), filename));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+        startActivityForResult(intent, PICK_FROM_CAMERA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode != RESULT_OK)
+            return;
+
+        switch(requestCode)
+        {
+            case PICK_FROM_ALBUM:
+                mImageUri = data.getData();
+                Log.d(TAG, "album" + mImageUri.getPath().toString());
+                //break;
+            case PICK_FROM_CAMERA:
+                Intent intent = new Intent("com.android.camera.action.CROP");
+                intent.setDataAndType(mImageUri, "image/*");
+                intent.putExtra("outputX", 200);
+                intent.putExtra("outputY", 200);
+                intent.putExtra("aspectX", 1);
+                intent.putExtra("aspectY", 1);
+                intent.putExtra("scale", true);
+                intent.putExtra("return-data", true);
+                startActivityForResult(intent, CROP_FROM_IMAGE);
+                break;
+            case CROP_FROM_IMAGE:
+                if(resultCode != RESULT_OK)
+                    return;
+                final Bundle extras = data.getExtras();
+
+                String filepath = Environment.getExternalStorageDirectory().getAbsolutePath()+
+                        "/LoginTest/tt_"+System.currentTimeMillis()+".jpg";
+                Log.d(TAG, filepath);
+
+                if(extras != null) {
+                    Bitmap photo = extras.getParcelable("data");
+                    mImageView.setImageBitmap(photo);
+                    saveImage(photo, filepath);
+                    mAbsoluteImagePath = filepath;
+                    break;
+                }
+
+                File file = new File(mImageUri.getPath());
+                if(file.exists())
+                    file.delete();
+
+                Log.d(TAG, "");
+                break;
+        }
+
+    }
+
+    private void saveImage(Bitmap bitmap, String filepath) {
+        String directoryPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/LoginTest";
+        File file = new File(directoryPath);
+
+        if(!file.exists())
+            file.mkdir();
+
+        File dstFile = new File(filepath);
+        BufferedOutputStream bos = null;
+
+        try {
+            dstFile.createNewFile();
+            bos = new BufferedOutputStream(new FileOutputStream(dstFile));
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(dstFile)));
+            bos.flush();
+            bos.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private void onRegister() {
         String mNickName = ((EditText)findViewById(R.id.et_nickname)).getText().toString();
         String mPassword1 = ((EditText)findViewById(R.id.et_pw_1)).getText().toString();
